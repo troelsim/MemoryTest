dc39662ab7899c96415e5303a553cc8dc2ff6500
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2017-11-27 14:19:37 +0100

    Initial

---
+++ MemoryTest/ChildPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace MemoryTest
+{
+	class ImageVM
+	{
+		public string Source { get; set; }
+	}
+
+	public class ChildPage : ContentPage
+	{
+		public ChildPage()
+		{
+			var template = new DataTemplate(typeof(ImageCell));
+			template.SetBinding(ImageCell.ImageSourceProperty, nameof(ImageVM.Source));
+			var listView = new ListView
+			{
+				ItemTemplate = template,
+				ItemsSource = Enumerable.Repeat(new ImageVM { Source = "icon.png" }, 20).ToList()
+			};
+			Content = listView;
+		}
+	}
+}
+
---
+++ MemoryTest/MemoryTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MemoryTest
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "MemoryTest",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}
+			};
+
+			var childPage = new ChildPage();
+			var NavigationPage = new NavigationPage(content);
+			MainPage = NavigationPage;
+
+			var childVisible = false;
+			Device.StartTimer(TimeSpan.FromSeconds(0.5), () =>
+			{
+				if (childVisible)
+				{
+					NavigationPage.PopAsync();
+				}
+				else
+				{
+					NavigationPage.PushAsync(childPage);
+				}
+				childVisible = !childVisible;
+				return true;
+			});
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
